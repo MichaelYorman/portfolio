b24bc90e8e54f13f7d59fdf778fe7d3a5d458d63
Jormanainen Henri 25462 <henri.jormanainen.henri.mikael@edu.riveria.fi>
2025-09-25 14:33:02 +0300

    developed crud actions

--- project/fashionguide/src/components/UserHome.jsx
+++ project/fashionguide/src/components/UserHome.jsx
@@ -11,24 +11,16 @@ const NavButton = styled(NavLink)`
 `;
 
 const CreateNewListDiv=styled.h1`
-text-align:center;
-&:hover{
-text-decoration:underline;
-}
+display:flex;
+justify-content:center;
+align-items:center;
+height:250px;
+width:250px;
 `;
 
 const NewListLogo=styled.img`
-width:50px;
+width:80px;
 height:auto;
-margin-right:60px;
-`;
-
-const ListingDiv2=styled.div`
-display:flex;
-flex-direction:row;
-justify-content:center;
-align-items:flex-end;
-margin:100px 10px 20px 10px;
 `;
 
 const Block=styled.div`
@@ -42,50 +34,71 @@ outline:5px dashed black;
 const ListingDiv=styled.div`
 display:flex;
 flex-direction:row;
+gap:20px;
 flex-wrap:wrap;
 padding:10px;
 margin-left:50px;
 margin-right:50px;
 margin-top:100px;
-width:80%;
+width:1100px;
 height:auto;
 outline-style:dashed;
 background-color:white;
 `;
+const ListButtonDiv=styled.div`
+display:flex;
+width:30px;
+height:30px;
+position:relative;
+background-color:blue;
+`
 function UserHome() {
   const [data,setData]=useState([]);
 
   useEffect(() => {
+    const controller= new AbortController(); //
     const fetchData=async()=>{
       try {
-        const response=await axios.get("http://localhost:3000/api/list");
-        setData(response.data);
+        const response=await axios.get("http://localhost:3000/api/list",{
+        signal:controller.signal
+      });
+        setData(response.data.data);
       } catch (err) {
+        if(err.name!=="CanceledError") {
         console.error("Error fetching data:",err);
+        }
       }
     };
     fetchData();
+    return () => controller.abort();
   },[]);
-
+  const handleDelete=async(id)=>{
+    try {
+      await axios.delete(`http://localhost:3000/api/list/${id}`);
+      setData(prev => prev.filter(item => item.id !== id));
+    } catch (err) {
+      console.error("Error deleting item:",err)
+    }
+  };
   const {t,setLang}=useTranslate();
   return (
     <>
       <div>
         <WidgetPanel/>
+        <ListingDiv>
         <CreateNewListDiv>
         <NavButton to="/new-list">
-        <NewListLogo src="/icons/pluscircle.svg" alt="Plus circle symbol"/>
-        {t("createlist")}</NavButton>
+        <NewListLogo src="/icons/pluscircle.svg" alt="Plus circle symbol"/></NavButton>
         </CreateNewListDiv>
-        <ListingDiv>
-          <Block>
-            <h4>You have nothing here yet...</h4>
-            </Block>
-            {data.map((item,index)=> (
+            {data.length >0 ? data.map(item => (
               <Block key={item.id}>
+                <ListButtonDiv onClick={()=>handleDelete(item.id)} src="icons/trashcan.svg"/>
                 <p>{item.list_name}</p>
               </Block>
-          ))}
+          )) : (
+            <Block>
+            <h4>You have nothing here yet...</h4>
+            </Block>)}
         </ListingDiv>
       </div>
     </>
